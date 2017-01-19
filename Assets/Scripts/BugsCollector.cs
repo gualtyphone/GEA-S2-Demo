33a237fba0c81a891d3a6dbbfd2784db0b28be93
KristopherH <k_hill_97@hotmail.com>
2017-01-19 17:23:55 +0000

    Cleaning up file layout
    FILES!!?!1

---
+++ Assets/Scripts/BugsCollector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugsCollector : MonoBehaviour {
+    public int collectedBugs = 0;
+    public GameObject door;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(collectedBugs >= 1)
+        {
+            door.GetComponent<PlatformMoving>().move = true;
+        }
+	}
+}
