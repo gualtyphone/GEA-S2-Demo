e841dd24e143d637d39dd8caba241da7813fb905
Gualtiero <gualtyphone@me.com>
2017-01-20 10:04:33 +0000

    Made trampoline Platform

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class Trampoline : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.gameObject.tag == "Player"){
+			other.gameObject.GetComponent<PlatformerCharacter2D>().m_jump = true;
+		}
+	}
+}
