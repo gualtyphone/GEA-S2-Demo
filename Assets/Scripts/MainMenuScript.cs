f0e0a53e736cca86f837450a701d7d23d33cd5f3
Daniel Donaldson <daniel2.donaldson@live.uwe.ac.uk>
2017-01-27 20:24:07 +0000

    demo screen draft

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void loadMainGame()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void loadLevelEditor()
+    {
+       
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
