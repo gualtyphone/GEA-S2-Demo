35d8a2dfee6fe3c8718fb4d7614b32e3de87d3a3
k9-hill <Kristopher2.Hill@live.uwe.ac.uk>
2017-02-01 17:48:32 +0000

    swing

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ladder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            col.GetComponent<Rigidbody2D>().gravityScale = 0;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                col.transform.Translate(new Vector3(0.0f, 0.1f, 0.0f));
+            }
+            else if(Input.GetKey(KeyCode.S))
+            {
+                col.transform.Translate(new Vector3(0.0f, -0.1f, 0.0f));
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            col.GetComponent<Rigidbody2D>().gravityScale = 3;
+        }
+    }
+}
---
+++ Assets/Scripts/Swing_Rope.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swing_Rope : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player" && col.transform.parent == null) col.transform.parent = this.transform;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Player" && col.transform.parent != null) col.transform.parent = null;
+    }
+}
